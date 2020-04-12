7214e32a6ecf8f75f6e4ecf96c008e8dc5a0da01
Paradox-3arthling <floydqaranja@gmail.com>
2020-04-12 22:43:45 +0300

    1st successful testing on handler

--- hook/hook.go
+++ hook/hook.go
@@ -1,11 +1,31 @@
 package hook
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
 // 1. func to for making hook server
 //    make sure it's https for safety
-func CreateServer(port, path string) (string, error){
-	new_str := port + path
-	// generate url to be used
+func CreateServer(port, path string) {
+	// (string, error) {
+	//new_str := port + path
 	// make server
-	return new_str, nil
+	http.HandleFunc(path, handler)
+	// mess := make(chan string)
+	log.Fatal(http.ListenAndServe(port, nil))
+	// return new_str, nil
 }
+
+// make handler made up of params to make url to be used
+
 // 2. func handler for setting up hook
+func handler(w http.ResponseWriter, r *http.Request) {
+	succ_mess := `{
+	"ResponseCode": "00000000",
+	"ResponseDesc": "success"
+	}`
+	fmt.Fprintf(w, succ_mess)
+	// fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
--- hook/hook_test.go
+++ hook/hook_test.go
@@ -1,17 +1,45 @@
 package hook
 
-import "testing"
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
 
 func TestHookWorking(t *testing.T) {
-	port := ":8080"
-	path := "/mpesa_hook"
-	url := "http://localhost" + port + path
-	url_got, err := CreateServer(port, path)
-	if err != nil {
-		t.Errorf("Got an error:%s\n", err)
-	}
-	if url_got != url {
-		t.Errorf("expected:%s, got:%s \n", url, url_got)
-	}
+	// port := ":8080"
+	// path := "/mpesa_hook"
+	// url := "http://localhost" + port + path
+	// url_got, err := CreateServer(port, path)
+	// if err != nil {
+	// 	t.Errorf("Got an error:%s\n", err)
+	// }
+	// if url_got != url {
+	// 	t.Errorf("expected->%s, got->%s \n", url, url_got)
+	// }
+
+	// make call back server
+	// t.Logf("Creating server")
+	// CreateServer(port, path)
+	ts := httptest.NewServer(http.HandlerFunc(handler))
+	defer ts.Close()
 	//call post web hook for tesing
+	test_inp := `{test: "abc"}`
+	resp, err := http.Post(ts.URL, "application/json", strings.NewReader(test_inp))
+	if err != nil {
+		t.Errorf("error posting:\n%s\n", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("error reading:\n%s\n", err)
+	}
+	t.Logf("body is:\n%s", string(body))
+	// succ_mess := `{
+	// 	"ResponseCode": "00000000",
+	// 	"ResponseDesc": "success"
+	// 	  "}`
 }
