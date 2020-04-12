fad88640e0f2c9c9d5fc6d826d97b916c7ebe03c
Paradox-3arthling <floydqaranja@gmail.com>
2020-04-12 13:04:52 +0300

    test for hook

--- hook/hook_test.go
+++ hook/hook_test.go
@@ -1,7 +1,16 @@
 package hook_test
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/paradox-3arthling/mpesa_api/hook"
+)
 
 func TestHookWorking(t *testing.T) {
-
+	port := ":8080"
+	url, err := hook.CreateServer(port, "/mpesa_hook")
+	if err != nil {
+		t.Errorf("err->%d", err)
+	}
+	//call post web hook for tesing
 }
