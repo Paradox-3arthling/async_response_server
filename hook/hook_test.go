99181cdff2457adff7ae87bda13762f791bd3b47
Paradox-3arthling <floydqaranja@gmail.com>
2020-04-14 15:46:36 +0300

    cleaning up some comments

--- hook/hook_test.go
+++ hook/hook_test.go
@@ -9,20 +9,6 @@ import (
 )
 
 func TestHookWorking(t *testing.T) {
-	// port := ":8080"
-	// path := "/mpesa_hook"
-	// url := "http://localhost" + port + path
-	// url_got, err := CreateServer(port, path)
-	// if err != nil {
-	// 	t.Errorf("Got an error:%s\n", err)
-	// }
-	// if url_got != url {
-	// 	t.Errorf("expected->%s, got->%s \n", url, url_got)
-	// }
-
-	// make call back server
-	// t.Logf("Creating server")
-	// CreateServer(port, path)
 	ts := httptest.NewServer(http.HandlerFunc(mpesaHandler))
 	defer ts.Close()
 	//call post web hook for tesing
